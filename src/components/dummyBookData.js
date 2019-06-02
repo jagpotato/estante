6eadcbc78d167527d05306001dd3c4e76332d564
daiki <dik.piyo.piyo@gmail.com>
2019-06-03 00:25:48 +0900

    update dummy

--- src/components/dummyBookData.js
+++ src/components/dummyBookData.js
@@ -1,5 +1,77 @@
 export const bookList = [
   {
+    name: '退屈なことはPythonにやらせよう',
+    isLending: false,
+    shopURL: 'https://www.amazon.co.jp/%E9%80%80%E5%B1%88%E3%81%AA%E3%81%93%E3%81%A8%E3%81%AFPython%E3%81%AB%E3%82%84%E3%82%89%E3%81%9B%E3%82%88%E3%81%86-%E2%80%95%E3%83%8E%E3%83%B3%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%BC%E3%81%AB%E3%82%82%E3%81%A7%E3%81%8D%E3%82%8B%E8%87%AA%E5%8B%95%E5%8C%96%E5%87%A6%E7%90%86%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-Al-Sweigart/dp/487311778X/ref=pd_sbs_14_3/357-3667057-3365403?_encoding=UTF8&pd_rd_i=487311778X&pd_rd_r=e33c73a4-6699-11e9-b2b2-652bfb501ec9&pd_rd_w=yxjYw&pd_rd_wg=X19lg&pf_rd_p=ad2ea29d-ea11-483c-9db2-6b5875bb9b73&pf_rd_r=J8R1PEBYGRTE3WGYTEG5&psc=1&refRID=J8R1PEBYGRTE3WGYTEG5',
+    thumbnailURL: 'http://books.google.com/books/content?id=ycqPDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    name: '退屈なことはPythonにやらせよう',
+    isLending: false,
+    shopURL: 'https://www.amazon.co.jp/%E9%80%80%E5%B1%88%E3%81%AA%E3%81%93%E3%81%A8%E3%81%AFPython%E3%81%AB%E3%82%84%E3%82%89%E3%81%9B%E3%82%88%E3%81%86-%E2%80%95%E3%83%8E%E3%83%B3%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%BC%E3%81%AB%E3%82%82%E3%81%A7%E3%81%8D%E3%82%8B%E8%87%AA%E5%8B%95%E5%8C%96%E5%87%A6%E7%90%86%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-Al-Sweigart/dp/487311778X/ref=pd_sbs_14_3/357-3667057-3365403?_encoding=UTF8&pd_rd_i=487311778X&pd_rd_r=e33c73a4-6699-11e9-b2b2-652bfb501ec9&pd_rd_w=yxjYw&pd_rd_wg=X19lg&pf_rd_p=ad2ea29d-ea11-483c-9db2-6b5875bb9b73&pf_rd_r=J8R1PEBYGRTE3WGYTEG5&psc=1&refRID=J8R1PEBYGRTE3WGYTEG5',
+    thumbnailURL: 'http://books.google.com/books/content?id=REi0DAAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    name: '退屈なことはPythonにやらせよう',
+    isLending: false,
+    shopURL: 'https://www.amazon.co.jp/%E9%80%80%E5%B1%88%E3%81%AA%E3%81%93%E3%81%A8%E3%81%AFPython%E3%81%AB%E3%82%84%E3%82%89%E3%81%9B%E3%82%88%E3%81%86-%E2%80%95%E3%83%8E%E3%83%B3%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%BC%E3%81%AB%E3%82%82%E3%81%A7%E3%81%8D%E3%82%8B%E8%87%AA%E5%8B%95%E5%8C%96%E5%87%A6%E7%90%86%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-Al-Sweigart/dp/487311778X/ref=pd_sbs_14_3/357-3667057-3365403?_encoding=UTF8&pd_rd_i=487311778X&pd_rd_r=e33c73a4-6699-11e9-b2b2-652bfb501ec9&pd_rd_w=yxjYw&pd_rd_wg=X19lg&pf_rd_p=ad2ea29d-ea11-483c-9db2-6b5875bb9b73&pf_rd_r=J8R1PEBYGRTE3WGYTEG5&psc=1&refRID=J8R1PEBYGRTE3WGYTEG5',
+    thumbnailURL: 'http://books.google.com/books/content?id=EZxUDgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    name: '退屈なことはPythonにやらせよう',
+    isLending: false,
+    shopURL: 'https://www.amazon.co.jp/%E9%80%80%E5%B1%88%E3%81%AA%E3%81%93%E3%81%A8%E3%81%AFPython%E3%81%AB%E3%82%84%E3%82%89%E3%81%9B%E3%82%88%E3%81%86-%E2%80%95%E3%83%8E%E3%83%B3%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%BC%E3%81%AB%E3%82%82%E3%81%A7%E3%81%8D%E3%82%8B%E8%87%AA%E5%8B%95%E5%8C%96%E5%87%A6%E7%90%86%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-Al-Sweigart/dp/487311778X/ref=pd_sbs_14_3/357-3667057-3365403?_encoding=UTF8&pd_rd_i=487311778X&pd_rd_r=e33c73a4-6699-11e9-b2b2-652bfb501ec9&pd_rd_w=yxjYw&pd_rd_wg=X19lg&pf_rd_p=ad2ea29d-ea11-483c-9db2-6b5875bb9b73&pf_rd_r=J8R1PEBYGRTE3WGYTEG5&psc=1&refRID=J8R1PEBYGRTE3WGYTEG5',
+    thumbnailURL: 'http://books.google.com/books/content?id=VbwpDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    name: '退屈なことはPythonにやらせよう',
+    isLending: false,
+    shopURL: 'https://www.amazon.co.jp/%E9%80%80%E5%B1%88%E3%81%AA%E3%81%93%E3%81%A8%E3%81%AFPython%E3%81%AB%E3%82%84%E3%82%89%E3%81%9B%E3%82%88%E3%81%86-%E2%80%95%E3%83%8E%E3%83%B3%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%BC%E3%81%AB%E3%82%82%E3%81%A7%E3%81%8D%E3%82%8B%E8%87%AA%E5%8B%95%E5%8C%96%E5%87%A6%E7%90%86%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-Al-Sweigart/dp/487311778X/ref=pd_sbs_14_3/357-3667057-3365403?_encoding=UTF8&pd_rd_i=487311778X&pd_rd_r=e33c73a4-6699-11e9-b2b2-652bfb501ec9&pd_rd_w=yxjYw&pd_rd_wg=X19lg&pf_rd_p=ad2ea29d-ea11-483c-9db2-6b5875bb9b73&pf_rd_r=J8R1PEBYGRTE3WGYTEG5&psc=1&refRID=J8R1PEBYGRTE3WGYTEG5',
+    thumbnailURL: 'http://books.google.com/books/content?id=WdydwwEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+  },
+  {
+    name: '退屈なことはPythonにやらせよう',
+    isLending: false,
+    shopURL: 'https://www.amazon.co.jp/%E9%80%80%E5%B1%88%E3%81%AA%E3%81%93%E3%81%A8%E3%81%AFPython%E3%81%AB%E3%82%84%E3%82%89%E3%81%9B%E3%82%88%E3%81%86-%E2%80%95%E3%83%8E%E3%83%B3%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%BC%E3%81%AB%E3%82%82%E3%81%A7%E3%81%8D%E3%82%8B%E8%87%AA%E5%8B%95%E5%8C%96%E5%87%A6%E7%90%86%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-Al-Sweigart/dp/487311778X/ref=pd_sbs_14_3/357-3667057-3365403?_encoding=UTF8&pd_rd_i=487311778X&pd_rd_r=e33c73a4-6699-11e9-b2b2-652bfb501ec9&pd_rd_w=yxjYw&pd_rd_wg=X19lg&pf_rd_p=ad2ea29d-ea11-483c-9db2-6b5875bb9b73&pf_rd_r=J8R1PEBYGRTE3WGYTEG5&psc=1&refRID=J8R1PEBYGRTE3WGYTEG5',
+    thumbnailURL: 'http://books.google.com/books/content?id=kOXDtAEACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+  },
+  {
+    name: '退屈なことはPythonにやらせよう',
+    isLending: true,
+    shopURL: 'https://www.amazon.co.jp/%E9%80%80%E5%B1%88%E3%81%AA%E3%81%93%E3%81%A8%E3%81%AFPython%E3%81%AB%E3%82%84%E3%82%89%E3%81%9B%E3%82%88%E3%81%86-%E2%80%95%E3%83%8E%E3%83%B3%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%BC%E3%81%AB%E3%82%82%E3%81%A7%E3%81%8D%E3%82%8B%E8%87%AA%E5%8B%95%E5%8C%96%E5%87%A6%E7%90%86%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-Al-Sweigart/dp/487311778X/ref=pd_sbs_14_3/357-3667057-3365403?_encoding=UTF8&pd_rd_i=487311778X&pd_rd_r=e33c73a4-6699-11e9-b2b2-652bfb501ec9&pd_rd_w=yxjYw&pd_rd_wg=X19lg&pf_rd_p=ad2ea29d-ea11-483c-9db2-6b5875bb9b73&pf_rd_r=J8R1PEBYGRTE3WGYTEG5&psc=1&refRID=J8R1PEBYGRTE3WGYTEG5',
+    thumbnailURL: 'http://books.google.com/books/content?id=vl2jDgAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    name: '退屈なことはPythonにやらせよう',
+    isLending: false,
+    shopURL: 'https://www.amazon.co.jp/%E9%80%80%E5%B1%88%E3%81%AA%E3%81%93%E3%81%A8%E3%81%AFPython%E3%81%AB%E3%82%84%E3%82%89%E3%81%9B%E3%82%88%E3%81%86-%E2%80%95%E3%83%8E%E3%83%B3%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%BC%E3%81%AB%E3%82%82%E3%81%A7%E3%81%8D%E3%82%8B%E8%87%AA%E5%8B%95%E5%8C%96%E5%87%A6%E7%90%86%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-Al-Sweigart/dp/487311778X/ref=pd_sbs_14_3/357-3667057-3365403?_encoding=UTF8&pd_rd_i=487311778X&pd_rd_r=e33c73a4-6699-11e9-b2b2-652bfb501ec9&pd_rd_w=yxjYw&pd_rd_wg=X19lg&pf_rd_p=ad2ea29d-ea11-483c-9db2-6b5875bb9b73&pf_rd_r=J8R1PEBYGRTE3WGYTEG5&psc=1&refRID=J8R1PEBYGRTE3WGYTEG5',
+    thumbnailURL: 'http://books.google.com/books/content?id=XOmADwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    name: '退屈なことはPythonにやらせよう',
+    isLending: true,
+    shopURL: 'https://www.amazon.co.jp/%E9%80%80%E5%B1%88%E3%81%AA%E3%81%93%E3%81%A8%E3%81%AFPython%E3%81%AB%E3%82%84%E3%82%89%E3%81%9B%E3%82%88%E3%81%86-%E2%80%95%E3%83%8E%E3%83%B3%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%BC%E3%81%AB%E3%82%82%E3%81%A7%E3%81%8D%E3%82%8B%E8%87%AA%E5%8B%95%E5%8C%96%E5%87%A6%E7%90%86%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-Al-Sweigart/dp/487311778X/ref=pd_sbs_14_3/357-3667057-3365403?_encoding=UTF8&pd_rd_i=487311778X&pd_rd_r=e33c73a4-6699-11e9-b2b2-652bfb501ec9&pd_rd_w=yxjYw&pd_rd_wg=X19lg&pf_rd_p=ad2ea29d-ea11-483c-9db2-6b5875bb9b73&pf_rd_r=J8R1PEBYGRTE3WGYTEG5&psc=1&refRID=J8R1PEBYGRTE3WGYTEG5',
+    thumbnailURL: 'http://books.google.com/books/content?id=3-9DMQAACAAJ&printsec=frontcover&img=1&zoom=1&source=gbs_api'
+  },
+  {
+    name: '退屈なことはPythonにやらせよう',
+    isLending: true,
+    shopURL: 'https://www.amazon.co.jp/%E9%80%80%E5%B1%88%E3%81%AA%E3%81%93%E3%81%A8%E3%81%AFPython%E3%81%AB%E3%82%84%E3%82%89%E3%81%9B%E3%82%88%E3%81%86-%E2%80%95%E3%83%8E%E3%83%B3%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%BC%E3%81%AB%E3%82%82%E3%81%A7%E3%81%8D%E3%82%8B%E8%87%AA%E5%8B%95%E5%8C%96%E5%87%A6%E7%90%86%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-Al-Sweigart/dp/487311778X/ref=pd_sbs_14_3/357-3667057-3365403?_encoding=UTF8&pd_rd_i=487311778X&pd_rd_r=e33c73a4-6699-11e9-b2b2-652bfb501ec9&pd_rd_w=yxjYw&pd_rd_wg=X19lg&pf_rd_p=ad2ea29d-ea11-483c-9db2-6b5875bb9b73&pf_rd_r=J8R1PEBYGRTE3WGYTEG5&psc=1&refRID=J8R1PEBYGRTE3WGYTEG5',
+    thumbnailURL: 'http://books.google.com/books/content?id=dTqUDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    name: '退屈なことはPythonにやらせよう',
+    isLending: false,
+    shopURL: 'https://www.amazon.co.jp/%E9%80%80%E5%B1%88%E3%81%AA%E3%81%93%E3%81%A8%E3%81%AFPython%E3%81%AB%E3%82%84%E3%82%89%E3%81%9B%E3%82%88%E3%81%86-%E2%80%95%E3%83%8E%E3%83%B3%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%BC%E3%81%AB%E3%82%82%E3%81%A7%E3%81%8D%E3%82%8B%E8%87%AA%E5%8B%95%E5%8C%96%E5%87%A6%E7%90%86%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-Al-Sweigart/dp/487311778X/ref=pd_sbs_14_3/357-3667057-3365403?_encoding=UTF8&pd_rd_i=487311778X&pd_rd_r=e33c73a4-6699-11e9-b2b2-652bfb501ec9&pd_rd_w=yxjYw&pd_rd_wg=X19lg&pf_rd_p=ad2ea29d-ea11-483c-9db2-6b5875bb9b73&pf_rd_r=J8R1PEBYGRTE3WGYTEG5&psc=1&refRID=J8R1PEBYGRTE3WGYTEG5',
+    thumbnailURL: 'http://books.google.com/books/content?id=SkuTDwAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
+    name: '退屈なことはPythonにやらせよう',
+    isLending: false,
+    shopURL: 'https://www.amazon.co.jp/%E9%80%80%E5%B1%88%E3%81%AA%E3%81%93%E3%81%A8%E3%81%AFPython%E3%81%AB%E3%82%84%E3%82%89%E3%81%9B%E3%82%88%E3%81%86-%E2%80%95%E3%83%8E%E3%83%B3%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9E%E3%83%BC%E3%81%AB%E3%82%82%E3%81%A7%E3%81%8D%E3%82%8B%E8%87%AA%E5%8B%95%E5%8C%96%E5%87%A6%E7%90%86%E3%83%97%E3%83%AD%E3%82%B0%E3%83%A9%E3%83%9F%E3%83%B3%E3%82%B0-Al-Sweigart/dp/487311778X/ref=pd_sbs_14_3/357-3667057-3365403?_encoding=UTF8&pd_rd_i=487311778X&pd_rd_r=e33c73a4-6699-11e9-b2b2-652bfb501ec9&pd_rd_w=yxjYw&pd_rd_wg=X19lg&pf_rd_p=ad2ea29d-ea11-483c-9db2-6b5875bb9b73&pf_rd_r=J8R1PEBYGRTE3WGYTEG5&psc=1&refRID=J8R1PEBYGRTE3WGYTEG5',
+    thumbnailURL: 'http://books.google.com/books/content?id=Np9SDQAAQBAJ&printsec=frontcover&img=1&zoom=1&edge=curl&source=gbs_api'
+  },
+  {
     name: 'ゼロから作るDeep Learning ―Pythonで学ぶディープラーニングの理論と実装',
     isLending: false,
     shopURL: 'https://www.amazon.co.jp/gp/product/4873117585?pf_rd_p=3d322af3-60ce-4778-b834-9b7ade73f617&pf_rd_r=11TS6TXVKJDJDA29DJF8',
